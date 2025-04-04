bd267d595a87bbf64dbdb0d8d01f86266812e65b
hololeac <hololeac@yopmail.com>
2025-04-04 21:49:20 +0300

    add nodes

--- src/main.py
+++ src/main.py
@@ -1,8 +1,29 @@
 from textnode import *
+from htmlnode import *
+
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    node0 = ParentNode(
+        "p",
+        [
+            LeafNode("b", "0000BOLDTEXT"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "0000italic text"),
+            LeafNode(None, "Normal text"),
+        ],
+    )
+    node = ParentNode(
+        "p",
+        [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+            node0
+        ],
+    )
+
+    print(node.to_html())
 
 if __name__ == '__main__':
     main()
